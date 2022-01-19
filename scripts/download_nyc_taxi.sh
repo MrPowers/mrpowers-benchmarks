2cd53ab5cacd3e93f07b5e0f23ed2e5d5ac9f8c0
MrPowers <matthewkevinpowers@gmail.com>
2022-01-19 06:31:15 -0500

    Update the script to download NYC taxi files

--- scripts/download_nyc_taxi.sh
+++ scripts/download_nyc_taxi.sh
@@ -1,11 +1,12 @@
 # Example usage: bash download_nyc_taxi 2009
 
 mkdir data/nyc-tlc
-#aws s3 cp s3://nyc-tlc/trip\ data/yellow_tripdata_2009-01.csv data/nyc-tlc
-#aws s3 cp s3://nyc-tlc/trip\ data/yellow_tripdata_2009-02.csv data/nyc-tlc
 
-for VARIABLE in 01 02 03 04 05 06 07 08 09 10 11 12
+for MONTH in 01 02 03 04 05 06 07 08 09 10 11 12
 do
-  aws s3 cp s3://nyc-tlc/trip\ data/yellow_tripdata_$1-$VARIABLE.csv data/nyc-tlc
+  aws s3 cp s3://nyc-tlc/trip\ data/yellow_tripdata_$1-$MONTH.csv data/nyc-tlc
 done
 
+# Example commands to download single files
+#aws s3 cp s3://nyc-tlc/trip\ data/yellow_tripdata_2009-01.csv data/nyc-tlc
+#aws s3 cp s3://nyc-tlc/trip\ data/yellow_tripdata_2009-02.csv data/nyc-tlc
