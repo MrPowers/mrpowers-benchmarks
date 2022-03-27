6595fb2e603979c44be06ec800501a63e7a7fb01
MrPowers <matthewkevinpowers@gmail.com>
2022-03-27 18:28:35 -0300

    Tweak the id3 logic

--- scripts/create_groupby_data.py
+++ scripts/create_groupby_data.py
@@ -13,25 +13,25 @@ def create_groupby_data(nrows, ngroups, output_dirname):
     # N & K are the variable names used in other scripts
     # nrows is N
     # ngroups is K
-    for g in range(1, ngroups+1):
-        f = open(f"{output_dirname}/demofile-{g}.csv", "a")
+    for group_num in range(1, ngroups+1):
+        f = open(f"{output_dirname}/mrpowers-groupby-{group_num:05}.csv", "a")
         f.write("id1,id2,id3,id4,id5,id6,v1,v2,v3\n")
-        id3_incrementor = random.randint(1,10)
-        id3 = 5
-        for i in range(1, int(nrows/ngroups)+1):
-            id3 = id3 + id3_incrementor
-            id2 = int(math.ceil(i * ngroups / nrows * ngroups))
-            id4 = random.randint(1,100)
+        for file_row in range(1, int(nrows/ngroups)+1):
+            id1 = group_num
+            id2 = int(math.ceil(file_row * ngroups / nrows * ngroups))
+            # N = file_row * ngroups
+            # K = ngroups
+            id3 = random.randint(file_row, nrows / ngroups)
+            id4 = random.randint(1, 100)
             id5 = random.randint(1, 100)
             id6 = random.randint(1, 10_000)
             v1 = random.randint(1, 5)
             v2 = random.randint(1, 15)
-            v3 = round(random.uniform(1,100), 6)
-            # TODO: The :03 needs to be updated with the number of digits in ngroups
-            f.write(f"id{g:03},id{id2:03},id{id3:0>10},{id4},{id5},{id6},{v1},{v2},{v3}\n")
+            v3 = round(random.uniform(1, 100), 6)
+            f.write(f"id{id1:03},id{id2:03},id{id3:0>10},{id4},{id5},{id6},{v1},{v2},{v3}\n")
         f.close()
 
-# TODO: make the tmp directory if it doesn't already exist
+# TODO: make the output_dirname directory if it doesn't already exist
 
 # 1e7
 create_groupby_data(10_000_000, 100, "tmp")
