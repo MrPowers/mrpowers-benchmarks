c25e45bef14a7a38449e0349531639a07336cd14
Matthew Powers <matthewkevinpowers@gmail.com>
2024-12-05 16:59:45 -0500

    move daft to parameterized queries

--- benchmarks/daft_h2o_groupby_queries.py
+++ benchmarks/daft_h2o_groupby_queries.py
@@ -1,57 +1,48 @@
 from helpers import benchmark, get_results
 import daft
+from daft.sql import SQLCatalog
 
 # print("setting native runner")
 # daft.context.set_runner_native()
 
 def q1(df):
-    x = df
-    return daft.sql("select id1, sum(v1) as v1 from x group by id1").collect()
+    return daft.sql("select id1, sum(v1) as v1 from x group by id1", catalog=SQLCatalog({"x": df})).collect()
 
 
 def q2(df):
-    x = df
-    return daft.sql("select id1, id2, sum(v1) as v1 from x group by id1, id2").collect()
+    return daft.sql("select id1, id2, sum(v1) as v1 from x group by id1, id2", catalog=SQLCatalog({"x": df})).collect()
 
 
 def q3(df):
-    x = df
-    return daft.sql("select id3, sum(v1) as v1, avg(v3) as v3 from x group by id3").collect()
+    return daft.sql("select id3, sum(v1) as v1, avg(v3) as v3 from x group by id3", catalog=SQLCatalog({"x": df})).collect()
 
 
 def q4(df):
-    x = df
-    return daft.sql("select id4, avg(v1) as v1, avg(v2) as v2, avg(v3) as v3 from x group by id4").collect()
+    return daft.sql("select id4, avg(v1) as v1, avg(v2) as v2, avg(v3) as v3 from x group by id4", catalog=SQLCatalog({"x": df})).collect()
 
 
 def q5(df):
-    x = df
-    return daft.sql("select id6, sum(v1) as v1, sum(v2) as v2, sum(v3) as v3 from x group by id6").collect()
+    return daft.sql("select id6, sum(v1) as v1, sum(v2) as v2, sum(v3) as v3 from x group by id6", catalog=SQLCatalog({"x": df})).collect()
 
 
 def q6(df):
-    x = df
-    return daft.sql("select id4, id5, median(v3) as median_v3, stddev(v3) as sd_v3 from x group by id4, id5").collect()
+    return daft.sql("select id4, id5, median(v3) as median_v3, stddev(v3) as sd_v3 from x group by id4, id5", catalog=SQLCatalog({"x": df})).collect()
 
 
 def q7(df):
-    x = df
-    return daft.sql("select id3, max(v1)-min(v2) as range_v1_v2 from x group by id3").collect()
+    return daft.sql("select id3, max(v1)-min(v2) as range_v1_v2 from x group by id3", catalog=SQLCatalog({"x": df})).collect()
 
 
 def q8(df):
-    x = df
-    return daft.sql("select id6, largest2_v3 from (select id6, v3 as largest2_v3, row_number() over (partition by id6 order by v3 desc) as order_v3 from x where v3 is not null) sub_query where order_v3 <= 2").collect()
+    return daft.sql("select id6, largest2_v3 from (select id6, v3 as largest2_v3, row_number() over (partition by id6 order by v3 desc) as order_v3 from x where v3 is not null) sub_query where order_v3 <= 2", catalog=SQLCatalog({"x": df})).collect()
 
 
 def q9(df):
-    x = df
-    return daft.sql("select id2, id4, power(corr(v1, v2), 2) as r2 from x group by id2, id4").collect()
+    return daft.sql("select id2, id4, power(corr(v1, v2), 2) as r2 from x group by id2, id4", catalog=SQLCatalog({"x": df})).collect()
 
 
 def q10(df):
-    x = df
-    return daft.sql("select id1, id2, id3, id4, id5, id6, sum(v3) as v3, count(*) as count from x group by id1, id2, id3, id4, id5, id6").collect()
+    return daft.sql("select id1, id2, id3, id4, id5, id6, sum(v3) as v3, count(*) as count from x group by id1, id2, id3, id4, id5, id6", catalog=SQLCatalog({"x": df})).collect()
 
 
 def run_benchmarks(df):
